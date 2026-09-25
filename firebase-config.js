3a247fd67b34a4dd979d232de1ea487ae7a080a2
a7medramadanarm-maker <a7medramadan.arm@gmail.com>
2026-09-25 21:33:21 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+// =========================================================
+// MindCare - Firebase Configuration
+// Prepared By Eng Ahmad Ramadan
+// =========================================================
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD6E8kmMBKOGlip9zACDYTTPEf54FxvBZ4",
+    authDomain: "mind-care-e7743.firebaseapp.com",
+    projectId: "mind-care-e7743",
+    storageBucket: "mind-care-e7743.firebasestorage.app",
+    messagingSenderId: "50815788969",
+    appId: "1:50815788969:web:0bcf23ec8b0a8b73a286b8",
+    measurementId: "G-K4FP94QFPH"
+};
+
+// Firebase configuration is initialized by the page
+// after Firebase SDK scripts are loaded.
+window.MINDCARE_FIREBASE_CONFIG = firebaseConfig;
